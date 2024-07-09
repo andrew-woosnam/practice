bf54ec98fd640c5a808aa89f1e93ecc04996fcb8
Andrew Woosnam <AndrewT.Woosnam@gmail.com>
2024-07-09 16:29:46 -0400

    ref(go): rewrite recursive binary search

--- go/binarysearch/binarysearch.go
+++ go/binarysearch/binarysearch.go
@@ -25,25 +25,21 @@ func BinarySearchIterative(collection []int, target int) int {
 
 // performs recursive binary search on a sorted array, returning the target's index if found, else -1
 func BinarySearchRecursive(collection []int, target int) int {
-	if len(collection) == 0 {
+	return recurse(collection, target, 0, len(collection)-1)
+}
+
+func recurse(collection []int, target int, start int, end int) int {
+	if start > end {
 		return -1
 	}
 
-	middle := len(collection) / 2
+	middle := start + (end-start)/2 // find mid without risking int overflow
 	sample := collection[middle]
 	if sample == target {
 		return middle
+	} else if sample < target {
+		return recurse(collection, target, middle+1, end)
+	} else { // sample > target
+		return recurse(collection, target, start, middle-1)
 	}
-
-	if target > sample {
-		newStart := middle + 1
-		result := BinarySearchRecursive(collection[newStart:], target)
-		if result == -1 {
-			return -1
-		}
-		return newStart + result
-	}
-
-	// target < sample
-	return BinarySearchRecursive(collection[:middle], target)
 }
