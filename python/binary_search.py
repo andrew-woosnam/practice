308f821b6554d21ec57241598e7d740ec7b2dedc
Andrew Woosnam <AndrewT.Woosnam@gmail.com>
2024-07-10 14:20:28 -0400

    feat(python): implement binary_search

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,16 @@
 def binary_search(arr, target):
+    l = 0
+    r = len(arr) - 1
+
+    while l <= r:
+        mid = l + (r-l)//2
+        sample = arr[mid]
+
+        if sample == target:
+            return mid
+        if sample < target:
+            l = mid + 1
+        elif sample > target:
+            r = mid - 1
+
     return -1
