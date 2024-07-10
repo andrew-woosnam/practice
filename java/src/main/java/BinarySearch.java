ad61efbee2b027c6684eb6579d19a8ce90d605f6
Andrew Woosnam <AndrewT.Woosnam@gmail.com>
2024-07-10 14:44:38 -0400

    feat(java): implement binary search

--- java/src/main/java/BinarySearch.java
+++ java/src/main/java/BinarySearch.java
@@ -1,7 +1,20 @@
 public class BinarySearch {
     public static int binarySearch(int[] arr, int target) {
-        // TODO: Implement binary search logic here
+        var l = 0;
+        var r = arr.length - 1;
 
-        return -1; // Placeholder return value
+        while (l <= r) {
+            var mid = l + (r - l) / 2;
+            var sample = arr[mid];
+
+            if (sample == target)
+                return mid;
+            if (sample < target)
+                l = mid + 1;
+            if (sample > target)
+                r = mid - 1;
+        }
+
+        return -1;
     }
 }
