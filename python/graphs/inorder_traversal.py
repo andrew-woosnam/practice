54c7126c3fe2e6604cb51b4815b142c8d976f299
Andrew Woosnam <AndrewT.Woosnam@gmail.com>
2024-07-11 14:23:09 -0400

    feat(inorder): implement inorder traversal in python

--- python/graphs/inorder_traversal.py
+++ python/graphs/inorder_traversal.py
@@ -11,5 +11,15 @@ class TreeNode:
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
+        stack = []
+        cur = root
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
 
         return res
