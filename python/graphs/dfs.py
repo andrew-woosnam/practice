64a4044f6b34127119093d3b8bf3c4d5c9e4b785
Andrew Woosnam <AndrewT.Woosnam@gmail.com>
2024-07-11 14:47:22 -0400

    ref(dfs): clean up python dfs impl

--- python/graphs/dfs.py
+++ python/graphs/dfs.py
@@ -1,22 +1,17 @@
 # assumes no duplicate nodes (each node str is unique)
 def traverse_dfs(graph: dict[str, list[str]], start: str) -> list[str]:
     res = []
-    to_visit = []
+    to_visit = [start]
     visited = set()
 
-    if len(graph) < 1:
+    if not graph:
         return res
-
-    if start not in graph:
-        return [start]
-
-    to_visit.append(start)
 
     while to_visit:
         cur = to_visit.pop()
-        res.append(cur)
-        visited.add(cur)
-        for next in graph[cur]:
-            if next not in visited and next not in to_visit:
-                to_visit.append(next)
+        if cur not in visited:
+            res.append(cur)
+            visited.add(cur)
+            for adjacent in graph.get(cur, []):
+                to_visit.append(adjacent)
     return res
