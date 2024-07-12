8a2e0574c58ed7e93f671ace1f824d92f806a9e1
Andrew Woosnam <AndrewT.Woosnam@gmail.com>
2024-07-11 21:37:35 -0400

    feat(bfs): implement bfs python

--- python/graphs/bfs.py
+++ python/graphs/bfs.py
@@ -1,5 +1,25 @@
 # assumes no duplicate nodes (each node str is unique)
+from collections import deque
+
+
 def traverse_bfs(graph: dict[str, list[str]], start: str) -> list[str]:
     res = []
-    # TODO: implement me
+    visited = set()
+    to_visit = deque()
+    to_visit.append(start)
+
+    if not graph:
+        return []
+
+    if start not in graph:
+        return [start]
+
+    while to_visit:
+        cur = to_visit.popleft()
+        if cur not in visited:
+            res.append(cur)
+            visited.add(cur)
+            for adjacent in graph.get(cur, []):
+                to_visit.append(adjacent)
+
     return res
